de3cdbc0c4aaa8679a398f88c3f4bd23d77b31dd
pranitakeshri <pranita1995keshri@gmail.com>
2017-02-14 19:01:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,7 @@
 console.log('Loaded!');
 var element = document.getElementById ('main-text');
 element.innerHTML = 'value changed';
+var img = document.getElementById('img');
+img.onclick=function(){
+    img.style.marginleft='100px';
+};
